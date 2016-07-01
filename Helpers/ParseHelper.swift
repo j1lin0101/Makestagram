0cf68dd743dbb34a7157db6b9eeb6e27c28d20d9
j1lin0101 <j1lin0101@gmail.com>
2016-07-01 12:36:26 -0700

    Fake users and stuff

--- Helpers/ParseHelper.swift
+++ Helpers/ParseHelper.swift
@@ -11,82 +11,133 @@ import Parse
 
 class ParseHelper {
         // Following Relation
-        static let ParseFollowClass       = "Follow"
-        static let ParseFollowFromUser    = "fromUser"
-        static let ParseFollowToUser      = "toUser"
+    static let ParseFollowClass       = "Follow"
+    static let ParseFollowFromUser    = "fromUser"
+    static let ParseFollowToUser      = "toUser"
         
         // Like Relation
-        static let ParseLikeClass         = "Like"
-        static let ParseLikeToPost        = "toPost"
-        static let ParseLikeFromUser      = "fromUser"
+    static let ParseLikeClass         = "Like"
+    static let ParseLikeToPost        = "toPost"
+    static let ParseLikeFromUser      = "fromUser"
         
         // Post Relation
-        static let ParsePostUser          = "user"
-        static let ParsePostCreatedAt     = "createdAt"
+    static let ParsePostUser          = "user"
+    static let ParsePostCreatedAt     = "createdAt"
         
         // Flagged Content Relation
-        static let ParseFlaggedContentClass    = "FlaggedContent"
-        static let ParseFlaggedContentFromUser = "fromUser"
-        static let ParseFlaggedContentToPost   = "toPost"
+    static let ParseFlaggedContentClass    = "FlaggedContent"
+    static let ParseFlaggedContentFromUser = "fromUser"
+    static let ParseFlaggedContentToPost   = "toPost"
         
         // User Relation
-        static let ParseUserUsername      = "username"
+    static let ParseUserUsername      = "username"
     
     static func timelineRequestForCurrentUser(range: Range<Int>, completionBlock: PFQueryArrayResultBlock) {
-            let followingQuery = PFQuery(className: ParseFollowClass)
-            followingQuery.whereKey(ParseFollowFromUser, equalTo:PFUser.currentUser()!)
-            
-            let postsFromFollowedUsers = Post.query()
-            postsFromFollowedUsers!.whereKey(ParsePostUser, matchesKey: ParseFollowToUser, inQuery: followingQuery)
-            
-            let postsFromThisUser = Post.query()
-            postsFromThisUser!.whereKey(ParsePostUser, equalTo: PFUser.currentUser()!)
-            
-            let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
-            query.includeKey(ParsePostUser)
-            query.orderByDescending(ParsePostCreatedAt)
+        let followingQuery = PFQuery(className: ParseFollowClass)
+        followingQuery.whereKey(ParseFollowFromUser, equalTo:PFUser.currentUser()!)
         
-            query.skip = range.startIndex
-        
-            query.limit = range.endIndex
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey(ParsePostUser, matchesKey: ParseFollowToUser, inQuery: followingQuery)
             
-            query.findObjectsInBackgroundWithBlock(completionBlock)
-        }
-        
-        static func likePost(user: PFUser, post: Post) {
-            let likeObject = PFObject(className: ParseLikeClass)
-            likeObject[ParseLikeFromUser] = user
-            likeObject[ParseLikeToPost] = post
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey(ParsePostUser, equalTo: PFUser.currentUser()!)
             
-            likeObject.saveInBackgroundWithBlock(nil)
-        }
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        query.includeKey(ParsePostUser)
+        query.orderByDescending(ParsePostCreatedAt)
     
-        static func unlikePost(user: PFUser, post: Post) {
-            // 1
-            let query = PFQuery(className: ParseLikeClass)
-            query.whereKey(ParseLikeFromUser, equalTo: user)
-            query.whereKey(ParseLikeToPost, equalTo: post)
+        query.skip = range.startIndex
+        
+        query.limit = range.endIndex
             
-            query.findObjectsInBackgroundWithBlock { (results: [PFObject]?, error: NSError?) -> Void in
+        query.findObjectsInBackgroundWithBlock(completionBlock)
+    }
+        
+    static func likePost(user: PFUser, post: Post) {
+        let likeObject = PFObject(className: ParseLikeClass)
+        likeObject[ParseLikeFromUser] = user
+        likeObject[ParseLikeToPost] = post
+            
+        likeObject.saveInBackgroundWithBlock(nil)
+    }
+    
+    static func unlikePost(user: PFUser, post: Post) {
+            // 1
+        let query = PFQuery(className: ParseLikeClass)
+        query.whereKey(ParseLikeFromUser, equalTo: user)
+        query.whereKey(ParseLikeToPost, equalTo: post)
+            
+        query.findObjectsInBackgroundWithBlock { (results: [PFObject]?, error: NSError?) -> Void in
                 // 2
-                if let results = results {
-                    for like in results {
-                        like.deleteInBackgroundWithBlock(nil)
-                    }
+            if let results = results {
+                for like in results {
+                    like.deleteInBackgroundWithBlock(nil)
                 }
             }
         }
+        }
     
         // 1
-        static func likesForPost(post: Post, completionBlock: PFQueryArrayResultBlock) {
-            let query = PFQuery(className: ParseLikeClass)
-            query.whereKey(ParseLikeToPost, equalTo: post)
-            // 2
-            query.includeKey(ParseLikeFromUser)
+    static func likesForPost(post: Post, completionBlock: PFQueryArrayResultBlock) {
+        let query = PFQuery(className: ParseLikeClass)
+        query.whereKey(ParseLikeToPost, equalTo: post)
+        // 2
+        query.includeKey(ParseLikeFromUser)
             
-            query.findObjectsInBackgroundWithBlock(completionBlock)
+        query.findObjectsInBackgroundWithBlock(completionBlock)
+    }
+    
+    static func getFollowingUsersForUser(user: PFUser, completionBlock: PFQueryArrayResultBlock) {
+        let query = PFQuery(className: ParseFollowClass)
+        
+        query.whereKey(ParseFollowFromUser, equalTo: user)
+        query.findObjectsInBackgroundWithBlock(completionBlock)
+    }
+    
+    static func addFollowRelationshipFromUser(user: PFUser, toUser: PFUser){
+        let followObject = PFObject(className: ParseFollowClass)
+        followObject.setObject(user, forKey: ParseFollowFromUser)
+        followObject.setObject(toUser, forKey: ParseFollowToUser)
+        
+        followObject.saveInBackgroundWithBlock(nil)
+    }
+    
+    static func removeFollowRelationshipFromUser(user: PFUser, toUser: PFUser){
+        let query = PFQuery(className: ParseFollowClass)
+        query.whereKey(ParseFollowFromUser, equalTo: user)
+        query.whereKey(ParseFollowToUser, equalTo: toUser)
+        
+        query.findObjectsInBackgroundWithBlock { (results: [PFObject]?, error: NSError?) -> Void in
+            let results = results ?? []
+            
+            for follow in results {
+                follow.deleteInBackgroundWithBlock(nil)
+            }
         }
     }
+    
+    static func allUsers(completionBlock: PFQueryArrayResultBlock) -> PFQuery{
+        let query = PFUser.query()!
+        query.whereKey(ParseHelper.ParseUserUsername, notEqualTo: PFUser.currentUser()!.username!)
+        query.orderByAscending(ParseHelper.ParseUserUsername)
+        query.limit = 20
+        query.findObjectsInBackgroundWithBlock(completionBlock)
+        
+        return query
+    }
+    
+    static func searchUsers(searchText: String, completionBlock: PFQueryArrayResultBlock) -> PFQuery {
+        let query = PFUser.query()!.whereKey(ParseHelper.ParseUserUsername, matchesRegex: searchText, modifiers: "i")
+        query.whereKey(ParseHelper.ParseUserUsername, notEqualTo: PFUser.currentUser()!.username!)
+        
+        query.orderByAscending(ParseHelper.ParseUserUsername)
+        query.limit = 20
+        
+        query.findObjectsInBackgroundWithBlock(completionBlock)
+        
+        return query
+    }
+}
 
 
 
